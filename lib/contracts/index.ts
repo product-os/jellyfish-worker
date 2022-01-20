2eeae4e9a6d30fbd37041d44a9527d2cd261659b
Josh Bowling <josh@balena.io>
2022-01-20 14:26:14 +0900

    Drop roles
    Most roles have been moved to jellyfish-core
    external-support role exists in plugin-default
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,8 +1,4 @@
 import { create } from './create';
-import { roleUserCommunity } from './role-user-community';
-import { roleUserExternalSupport } from './role-user-external-support';
-import { roleUserOperator } from './role-user-operator';
-import { roleUserTest } from './role-user-test';
 import { scheduledAction } from './scheduled-action';
 import { triggeredAction } from './triggered-action';
 import { update } from './update';
@@ -12,10 +8,6 @@ import { viewAllViews } from './view-all-views';
 export default {
 	create,
 	'triggered-action': triggeredAction,
-	'role-user-community': roleUserCommunity,
-	'role-user-external-support': roleUserExternalSupport,
-	'role-user-operator': roleUserOperator,
-	'role-user-test': roleUserTest,
 	'scheduled-action': scheduledAction,
 	update,
 	'view-all-views': viewAllViews,
