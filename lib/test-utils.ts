156292362f08e39c7273ee3db731b9f5e78e89c0
karaxuna <kaxabernet@gmail.com>
2022-02-02 16:11:07 +0400

    Allow regexp in base url (webhookScenario)
    Change-type: patch

--- lib/test-utils.ts
+++ lib/test-utils.ts
@@ -594,7 +594,7 @@ export async function webhookScenario(
 	},
 	options: {
 		source: string;
-		baseUrl: string;
+		baseUrl: string | RegExp;
 		uriPath: RegExp;
 		basePath: string;
 		isAuthorized: (request: any) => any;
