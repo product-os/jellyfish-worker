bceea4396bf7a93a297071166c33a3579651585d
Josh Bowling <josh@balena.io>
2022-02-22 02:39:31 +0000

    Fix contract export
    We should avoid exporting with default and
    instead match other modules by exporting
    a typed, named variable.
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,3 +1,4 @@
+import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
 import { create } from './create';
 import { scheduledAction } from './scheduled-action';
 import { triggeredAction } from './triggered-action';
@@ -5,12 +6,11 @@ import { update } from './update';
 import { viewAllViews } from './view-all-views';
 import { viewScheduledActions } from './view-scheduled-actions';
 
-// TS-TODO: Load these contracts from a model repo
-export default {
+export const contracts: ContractDefinition[] = [
 	create,
-	'triggered-action': triggeredAction,
-	'scheduled-action': scheduledAction,
+	scheduledAction,
+	triggeredAction,
 	update,
-	'view-all-views': viewAllViews,
-	'view-scheduled-actions': viewScheduledActions,
-};
+	viewAllViews,
+	viewScheduledActions,
+];
