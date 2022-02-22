6e0b7caef26509d6d317cacaf35fc1aecf516480
Josh Bowling <josh@balena.io>
2022-02-22 02:40:32 +0000

    Add scheduled actions view
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -3,6 +3,7 @@ import { scheduledAction } from './scheduled-action';
 import { triggeredAction } from './triggered-action';
 import { update } from './update';
 import { viewAllViews } from './view-all-views';
+import { viewScheduledActions } from './view-scheduled-actions';
 
 // TS-TODO: Load these contracts from a model repo
 export default {
@@ -11,4 +12,5 @@ export default {
 	'scheduled-action': scheduledAction,
 	update,
 	'view-all-views': viewAllViews,
+	'view-scheduled-actions': viewScheduledActions,
 };
