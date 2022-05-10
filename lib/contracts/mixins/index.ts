bfb3c8abb955511608473524406fa0193d128464
Josh Bowling <josh@balena.io>
2022-05-10 14:38:52 +0900

    Add agent-settings contract, timezone mixin
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/mixins/index.ts
+++ lib/contracts/mixins/index.ts
@@ -6,6 +6,7 @@ import { contractMixins } from 'autumndb';
 import _ from 'lodash';
 
 export { asPipelineItem } from './as-pipeline-item';
+export { asTimeZone } from './as-time-zone';
 export { withEvents } from './with-events';
 
 export const mixin = (...mixins: ContractDefinition[]) => {
