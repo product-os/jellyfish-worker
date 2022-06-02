32684900505d0bf516ad5516f784ffde9fc39381
Josh Bowling <josh@balena.io>
2022-06-02 06:08:53 +0000

    Fix action export format
    Update action export format to
    match that of other libraries
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,4 @@
 import type { ActionDefinition } from '../plugin';
-import type { Map } from '../types';
 import { actionCreateCard } from './action-create-card';
 import { actionCreateEvent } from './action-create-event';
 import { actionCreateSession } from './action-create-session';
@@ -8,12 +7,12 @@ import { actionMatchMakeTask } from './action-matchmake-task';
 import { actionSetAdd } from './action-set-add';
 import { actionUpdateCard } from './action-update-card';
 
-export const actions: Map<ActionDefinition> = {
-	'action-create-card': actionCreateCard,
-	'action-create-event': actionCreateEvent,
-	'action-create-session': actionCreateSession,
-	'action-create-user': actionCreateUser,
-	'action-matchmake-task': actionMatchMakeTask,
-	'action-set-add': actionSetAdd,
-	'action-update-card': actionUpdateCard,
-};
+export const actions: ActionDefinition[] = [
+	actionCreateCard,
+	actionCreateEvent,
+	actionCreateSession,
+	actionCreateUser,
+	actionMatchMakeTask,
+	actionSetAdd,
+	actionUpdateCard,
+];
